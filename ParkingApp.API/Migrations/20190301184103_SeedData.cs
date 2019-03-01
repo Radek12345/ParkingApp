552283e6bab253334c3aefa27a80bd0f8122294b
Radek12345 <gosciminskiradoslaw@gmail.com>
2019-03-01 20:00:13 +0100

    Seed Database

---
+++ ParkingApp.API/Migrations/20190301184103_SeedData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkingApp.API.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO ParkingAreas 
+                (name, WeekdaysHourlyRate, WeekendHourlyRate) 
+                VALUES ('TestArea1', 4, 3)");
+
+            migrationBuilder.Sql(@"INSERT INTO ParkingAreas 
+                (name, WeekdaysHourlyRate, WeekendHourlyRate) 
+                VALUES ('TestArea2', 4.50, 3.60)");
+
+            migrationBuilder.Sql(@"INSERT INTO ParkingAreas 
+                (name, WeekdaysHourlyRate, WeekendHourlyRate, Discount) 
+                VALUES ('TestArea3', 4.50, 3.60, 0.50)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM ParkingAreas WHERE name LIKE 'TestArea%'");
+        }
+    }
+}
