5067f57d92e39964993da45a95ec64f62f7bfcc5
Radek12345 <gosciminskiradoslaw@gmail.com>
2019-03-03 16:14:06 +0100

    Completed areas table view

--- ParkingApp-SPA/src/app/components/management/management.component.ts
+++ ParkingApp-SPA/src/app/components/management/management.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ParkingArea } from 'src/app/models/parking-area';
+import { ParkingAreaService } from './../../services/parking-area.service';
+
 @Component({
   selector: 'app-management',
   templateUrl: './management.component.html',
   styleUrls: ['./management.component.css']
 })
 export class ManagementComponent implements OnInit {
+  private _parkingAreas: ParkingArea[];
 
-  constructor() { }
+  constructor(private parkingAreaService: ParkingAreaService) { }
 
   ngOnInit() {
+    this.loadParkingAreas();
+  }
+
+  private loadParkingAreas() {
+    this.parkingAreaService.getParkingAreas().subscribe(parkingAreas => {
+      this._parkingAreas = parkingAreas;
+    });
+  }
+
+  get parkingAreas() {
+    return this._parkingAreas;
   }
 
 }
