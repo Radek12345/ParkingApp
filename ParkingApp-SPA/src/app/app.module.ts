e7420717e40fe7b7e05dc396ba05642511bc3916
Radek12345 <gosciminskiradoslaw@gmail.com>
2019-03-03 14:16:09 +0100

    Working on management view

--- ParkingApp-SPA/src/app/app.module.ts
+++ ParkingApp-SPA/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { BsDatepickerModule } from 'ngx-bootstrap';
 import { TimepickerModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ParkingAreaService } from './services/parking-area.service';
 import { PaymentComponent } from './components/payment/payment.component';
-import { FormsModule } from '@angular/forms';
+import { ManagementComponent } from './components/management/management.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaymentComponent
+    PaymentComponent,
+    ManagementComponent
   ],
   imports: [
     BrowserModule,
